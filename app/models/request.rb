fdc3e58b2bdfd7441a421a8c56e1dcea9512cbd6
Walter Han <walehan@gmail.com>
2014-06-20 07:46:26 -0500

    Minor fix

--- app/models/request.rb
+++ app/models/request.rb
@@ -8,11 +8,12 @@ class Request < ActiveRecord::Base
   belongs_to :event_type
   belongs_to :request_type  
 
-
-  def add_person(person, communication_method_id)
+  # TODO - add logic to make sure owner has answered already, if person != creator_person
+  def add_person_request(person, communication_method_id)
     pr = PersonRequest.new :communication_method_id => communication_method_id
     pr.person = person
     self.person_requests << (pr)
     self.save
   end
+
 end
